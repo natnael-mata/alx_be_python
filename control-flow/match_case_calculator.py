373aa5fe6f8cdd29286da156e674e833007275da
natnael-mata <nati.sami.mata@gmail.com>
2025-09-29 20:21:28 +0300

    first match case submition

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,23 @@
+# match case calculator
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+opr = input("Choose the operation (+, -, *, /): ")
+result =0
+
+match opr:
+    case "+":
+        result = num1 + num2    
+    case "-":
+        result = num1 - num2
+    case "*":
+        result = num1 * num2
+    case "/":   
+        if num2 <= 0:
+            print("Cannot divide by zero.") 
+            exit() 
+        result = num1 / num2
+    case _:
+        print("invalid Oprator")
+print(f"The result is {result}")
+    
