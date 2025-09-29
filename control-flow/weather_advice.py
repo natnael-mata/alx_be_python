0174e445a1e81831eda0b21dd9c80bb18e3ee818
natnael-mata <nati.sami.mata@gmail.com>
2025-09-29 19:54:03 +0300

    second clothing recommendations

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,10 +1,11 @@
-#
-weatherInput = input("What's the weather like today? (sunny/rainy/cold): ")
+# close recommendations
+weather = input("What's the weather like today? (sunny/rainy/cold): ").strip().lower()
 
-if weatherInput == "sunny":
+if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
-elif weatherInput == "rainy":
+elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif weatherInput == "cold":
+elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
-else: print("Sorry, I don't have recommendations for this weather.")
+else: 
+    print("Sorry, I don't have recommendations for this weather.")
