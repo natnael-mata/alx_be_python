3cd4bbf911e8f9c690d52e20fa1814199bddf5f9
natnael-mata <nati.sami.mata@gmail.com>
2025-09-29 20:26:05 +0300

    second match case submitin

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -13,11 +13,12 @@ match opr:
     case "*":
         result = num1 * num2
     case "/":   
-        if num2 <= 0:
+        if num2 == 0:
             print("Cannot divide by zero.") 
             exit() 
         result = num1 / num2
     case _:
         print("invalid Oprator")
+        exit()
 print(f"The result is {result}")
     
