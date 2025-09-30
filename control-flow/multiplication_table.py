1b0d9347ffb27d31474b6a8b6e5c9c3c8fadc256
natnael-mata <nati.sami.mata@gmail.com>
2025-09-30 11:21:39 +0300

    first commit multiplication table

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,6 @@
+# multiplication table using for loop
+
+number = int(input("Enter a number to see its multiplication table: "))
+
+for num in range(1,11):  
+    print(f"{number} * {num} = {number*num}")
