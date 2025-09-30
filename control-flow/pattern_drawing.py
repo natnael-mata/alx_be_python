cbfeb5e2925bb39841c229304ff283013cf2b1e4
natnael-mata <nati.sami.mata@gmail.com>
2025-09-30 12:54:11 +0300

    second pattern drawing

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,7 +1,8 @@
 #pattern drawing
 length = int(input("Enter the size of the pattern: "))
-
-for size in range(0,length):
+raw=0
+while raw<length:
     for size in range(0,length):
         print("*", end="")
     print()
+    raw+=1
