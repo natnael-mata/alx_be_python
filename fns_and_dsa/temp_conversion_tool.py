d6ee48430bb80f4f755e8418a2f74fd16dbc7d69
natnael-mata <nati.sami.mata@gmail.com>
2025-10-15 19:46:56 +0300

    second temprature commit

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,29 +1,29 @@
-# tepmrature 
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 
 def convert_to_celsius(fahrenheit): 
-     return ((fahrenheit - 32)*FAHRENHEIT_TO_CELSIUS_FACTOR)
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 def convert_to_fahrenheit(celsius):
-    return ((celsius*CELSIUS_TO_FAHRENHEIT_FACTOR)+32)
+    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
 def main():
-    temperature = input("Enter the temperature to convert: ")
+    temperature_input = input("Enter the temperature to convert: ")
 
     try:
-        temperature = float(temperature)
+        temperature = float(temperature_input)
     except ValueError:
-        print("Invalid Input Value")
-        return 
-    choise = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
-    if choise == 'F':
+        print("Invalid temperature. Please enter a numeric value.")
+        return
+
+    choice = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").upper()
+
+    if choice == 'F':
         print(f"{temperature} °F is {convert_to_celsius(temperature)} °C")
-    elif choise == 'C':
+    elif choice == 'C':
         print(f"{temperature} °C is {convert_to_fahrenheit(temperature)} °F")
-    else: 
-        print("invalid Choise ID")
+    else:
+        print("Invalid choice")
 
-
-main()
-        
+if __name__ == "__main__":
+    main()
