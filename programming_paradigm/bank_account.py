b50d00b26177224b8d177f0761e06c1cd7d16e47
natnael-mata <nati.sami.mata@gmail.com>
2025-10-16 18:33:42 +0300

    first push bank account

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,17 @@
+#OOP
+class BankAccount():
+    def __init__(self, inital_balance=0):
+        self.account_balance = inital_balance
+                
+    def deposit(self, amount):
+        self.account_balance +=amount    
+    
+    def withdraw(self, amount):
+        if amount <= self.account_balance:
+            self.account_balance -=amount
+            return True
+        else:
+            return False
+        
+    def display_balance(self):
+        print(f"Current balance: ${self.account_balance}")
