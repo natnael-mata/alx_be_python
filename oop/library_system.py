a1217ee1247e465b64fe08415c628836c258f155
natnael-mata <nati.sami.mata@gmail.com>
2025-10-23 18:56:17 +0300

    first push

---
+++ oop/library_system.py
@@ -0,0 +1,38 @@
+class Book:
+
+    def __init__(self, title: str, author:str):
+        self.title = title
+        self.author = author
+    
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+class EBook(Book):
+
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size = file_size
+
+    def __str__(self):
+        return f"{self.title} by {self.author}, File size: {self.file_size}MB"
+
+class PrintBook(Book):
+
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
+        self.page_count = page_count
+    
+    def __str__(self):
+        return f"{self.title} by {self.author}, Pages: {self.page_count}"
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, books):
+        self.books.append(books)
+    
+    def list_books(self):
+        for book in self.books:
+            print(book)
+        
