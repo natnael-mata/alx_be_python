5cb32bb675e3e3349c92ed6b2296d79f1849e02e
natnael-mata <nati.sami.mata@gmail.com>
2025-10-23 18:58:21 +0300

    second push

--- oop/library_system.py
+++ oop/library_system.py
@@ -5,7 +5,7 @@ class Book:
         self.author = author
     
     def __str__(self):
-        return f"{self.title} by {self.author}"
+        return f"Book: {self.title} by {self.author}"
 
 class EBook(Book):
 
@@ -14,7 +14,7 @@ class EBook(Book):
         self.file_size = file_size
 
     def __str__(self):
-        return f"{self.title} by {self.author}, File size: {self.file_size}MB"
+        return f"EBook: {self.title} by {self.author}, File size: {self.file_size}MB"
 
 class PrintBook(Book):
 
@@ -23,7 +23,7 @@ class PrintBook(Book):
         self.page_count = page_count
     
     def __str__(self):
-        return f"{self.title} by {self.author}, Pages: {self.page_count}"
+        return f"PrintBook: {self.title} by {self.author}, Pages: {self.page_count}"
 
 class Library:
     def __init__(self):
